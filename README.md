7fcb1a1d9d7f7d206e401506b2123b3a1df66928
Musa Elqaq <melqa2@uic.edu>
2024-01-25 11:53:15 -0600

    Command 2 Completed

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# Version 1.1R - Completed Command 1
+# Version 2.1R - Completed Command 2
 
 > This version currently supports the following functionality:
 >
->    - The use of 'x' to exit the program, upon input from the user  
->    - Searching for exact search terms in the database works correctly  
->    - Statistics are accurately outputted to the user prior to the first command prompt
+>    - Everything from version 1.1 - 2.1
+>    - Searching for data/percentages on a specific station [see [changelog](#changelog)]
+>
 
 ------
-# Changelog:
-
->    - Added functionality to support partial input searches, so searching something like
->       "O'Hare" would work.  Naturally, anything that has that term in it will also be shown,
->       but this works as intended right now.  [Ex: Searching "O'Hare" would get you several
->       stations that stop at O'Hare, even if you only wanted "O'Hare Airport" specifically.]
+# Changelog:  ##
+> 
+>    - Given a station name, finds the percentage of riders on weekdays, on Saturdays, and on
+>      Sundays/holidays for that station. 
+>       - *The station name from the user should be an _**exact**_ match.*
+>    - Each percentage is calculated out of the total number of riders for that station
+>    - Displays both the totals and the percentages.
 >
->    -  **Final Update For Command 1**
+>    -  **Final Update For Command 2**
 
 ------
 
@@ -22,11 +23,10 @@
 
 > The next version will focus on:
 >
->   - Adding functionality for **command 2**, specifically:  
-      ```Given a station name, find the percentage of riders on weekdays, on Saturdays, and on
-         Sundays/holidays for that station. The station name from the user should be an exact 
-         match. Each percentage should be calculated out of the total number of riders for that 
-         station. Display both the totals and the percentages```
+>   - Adding functionality for **command 3**, specifically:  
+      ```Output the total ridership on weekdays for each station, with station names rather than 
+         the station IDs. Also show the percentages, taken out of the total ridership on weekdays 
+         for all the stations. Order the results in descending order by ridership.```
 
 ------
 
@@ -34,10 +34,10 @@
 
 > Current progress:
 >
-> - [ ] Given a name, find the percentages[^1] on weekdays
-> - [ ] Given a name, find the percentages[^1] on Saturdays
-> - [ ] Given a name, find the percentages[^1] on Sundays/holidays
-> - [ ] Display both the totals and percentages calculated
+> - [x] Given a name, find the percentages[^1] on weekdays
+> - [x] Given a name, find the percentages[^1] on Saturdays
+> - [x] Given a name, find the percentages[^1] on Sundays/holidays
+> - [x] Display both the totals and percentages calculated
 
 ------
 
--- main.py
+++ main.py
@@ -4,37 +4,66 @@
 
 import sqlite3
 import string
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 ################################################################## 
 #
 # search_Station
 #
-# Search for the given station name in the database
+# Search for the given station name in the database.
+# If mode == 0 , enables exact general search term only
+# If mode == 1 , enables partial general search
+# If mode == 2 , returns the station_ID
 #
-def search_Station(dbConn, searchStation):
+def search_Station(dbConn, searchStation, mode):
     dbCursor = dbConn.cursor()
 
     # print("\nSearching for " + searchStation + " in database...\n")
+
+    # Search database for station and order them alphabetically
+    if (mode == 0):  # Exact matches only
+        dbCursor.execute("SELECT Station_Name FROM Stations WHERE Stations.Station_Name = ? ORDER BY Stations.Station_Name ASC;", (searchStation,))
     
-    # Search for the stations and order them alphabetically
-    dbCursor.execute("SELECT Station_Name, Station_ID FROM Stations WHERE Stations.Station_Name LIKE ? ORDER BY Stations.Station_Name ASC;", (searchStation,))
+    elif (mode == 1): # Search for station(s) using partial names
+        dbCursor.execute("SELECT Station_Name, Station_ID FROM Stations WHERE Stations.Station_Name LIKE ? ORDER BY Stations.Station_Name ASC;", (searchStation,))
+
+    elif (mode == 2): # Exact matche search for given station's ID
+        dbCursor.execute("SELECT Station_ID FROM Stations WHERE Stations.Station_Name = ? ORDER BY Stations.Station_Name ASC;", (searchStation,))
 
     # Get the result
     rows = dbCursor.fetchall()
 
     # Check if any results are returned
-    if rows:
-        # If there are matches
-        for row in rows:
-            found_station = row[0]
-            station_ID = row[1]
-            print(station_ID, ": " + found_station)
+    if (rows):
+
+        if (mode == 0):
+            # If there are matches
+            for row in rows:
+                found_station = row[0]
+                station_ID = row[1]
+                print(station_ID, ": " + found_station)
+            
+            return [row[0] for row in rows]
+        
+        elif (mode == 1):
+            # If there are matches
+            for row in rows:
+                found_station = row[0]
+                station_ID = row[1]
+                print(station_ID, ": " + found_station)
+            
+            return [row[0] for row in rows]
 
         # print("\nNumber of matching stations: ", len(rows))
-        # You can save the results to a variable or return them as needed
-        return [row[0] for row in rows]
+        elif (mode == 2):
+            
+            for row in rows:
+                station_ID = row[0]
+                # print(station_ID, ": " + searchStation)
+
+            return [row[0] for row in rows]
+        
     else:
         # If no match is found, handle it
         # print("Station not found in the database.")
@@ -55,14 +84,103 @@ def partial_Name_Search(dbConn):
     query = input("Enter partial station name (wildcards _ and %): ")
 
     # Call search_Station() function
-    result = search_Station(dbConn, query)
+    result = search_Station(dbConn, query, 1)
 
-    if result == None:
+    if (result == None):
         print("**No stations found...")
 
     print() # Formatting
 
     # End partial_Name_Search()
+
+
+################################################################## 
+#
+# station_Search_Percentages
+#
+# Search for the given *exact* station name in the database
+# Output station names in ascending order. If no stations are 
+# found, print a message indicating that no stations were found.
+# Finds the percentage of riders on weekdays, on Saturdays, and on
+# Sundays/holidays for that station. Percentage calculated out of 
+# the total number of riders for that station. Displays both the 
+# totals and the percentages.
+# 
+def station_Search_Percentages(dbConn):
+    
+    # Get input from user
+    query = input("Enter the name of the station you would like to analyze: ")
+
+    # Call search_Station() function to find the station's ID
+    stationID = search_Station(dbConn, query, 2)
+
+    if (stationID == None):
+        print("**No data found...\n")
+        return None
+    
+    # Get the station ID from the list
+    stationID = stationID[0]
+
+    dbCursor = dbConn.cursor()
+
+    # Query for weekday riders
+    dbCursor.execute("SELECT SUM(Num_Riders) AS Weekday_Rider_Sum FROM Ridership WHERE Type_of_Day = 'W' AND station_ID = ?;", (stationID,))
+    weekdayRidersQuery = dbCursor.fetchone()
+
+    # Query for total riders
+    dbCursor.execute("SELECT SUM(Num_Riders) AS Total_Rider_Sum FROM Ridership WHERE station_ID = ?;", (stationID,))
+    totalRidersQuery = dbCursor.fetchone()
+
+    # Query for sunday riders
+    dbCursor.execute("SELECT SUM(Num_Riders) AS Saturday FROM Ridership WHERE Type_of_Day = 'A' AND station_ID = ?;", (stationID,))
+    saturdayRidersQuery = dbCursor.fetchone()
+
+    # Query for saturday riders
+    dbCursor.execute("SELECT SUM(Num_Riders) AS Sunday_Rider_Sum FROM Ridership WHERE Type_of_Day = 'U' AND station_ID = ?;", (stationID,))    
+    sundayRidersQuery = dbCursor.fetchone()
+
+    # Check if any results are returned
+    if weekdayRidersQuery and totalRidersQuery and sundayRidersQuery and saturdayRidersQuery:
+
+        # Get the results from the queries
+        weekdayRiders = weekdayRidersQuery[0]
+        totalRiders = totalRidersQuery[0]
+        sundayRiders = sundayRidersQuery[0]
+        saturdayRiders = saturdayRidersQuery[0]
+
+        # Calculate the percentage
+        weekDayRiderPercent = (weekdayRiders/totalRiders) * 100
+        sundayRidersPercent = (sundayRiders/totalRiders) * 100
+        saturdayRidersPercent = (saturdayRiders/totalRiders) * 100
+
+        # Print or use the results as needed
+        print(f"Weekday Ridership: ", f"{weekdayRiders:,}", f"({weekDayRiderPercent:.2f}%)")
+        print(f"Saturday ridership: {saturdayRiders:,}", f"({saturdayRidersPercent:.2f}%)") 
+        print(f"Sunday/holiday ridership: {sundayRiders:,}", f"({sundayRidersPercent:.2f}%)") 
+        print(f"Total Ridership: {totalRiders:,}")
+
+    # Get the result
+    rows = dbCursor.fetchone()
+
+    # Check if any results are returned
+    # if (rows):
+        
+    #     print("\nData found for station", stationID, "!")
+
+        # if (rows[0] != None):
+        #     print("Number of riders:", rows[0])
+        
+        # If there are matches
+        # for row in rows:
+        #     weekdayRiders = row[0]
+        #     print(stationID, ": ", weekdayRiders)
+        
+        # return [row[0] for row in rows]
+    
+    print() # Formatting
+
+    # End station_Search_Percentages()
+
 
 
 ##################################################################  
@@ -99,7 +217,7 @@ def print_stats(dbConn):
     row = dbCursor.fetchone();
 
     # Check if a result is returned; prevented some file issues
-    if row:
+    if (row):
         min_date = row[0]
         max_date = row[1]
 
@@ -126,48 +244,47 @@ def print_stats(dbConn):
 def commandDriver(userChoice, dbConn):
     
     # If the input is 1-9, execute the relevant processes
-    if userChoice == '1':
+    if (userChoice == '1'):
         partial_Name_Search(dbConn)
 
-    elif userChoice == '2':
-        print("Chose command 2 - Not Yet Implemented.\nExiting...\n")
-        exit(0)
+    elif (userChoice == '2'):
+        station_Search_Percentages(dbConn)
 
-    elif userChoice == '3':
+    elif (userChoice == '3'):
         print("Chose command 3 - Not Yet Implemented.\nExiting...\n")
         exit(0)
 
-    elif userChoice == '4':
+    elif (userChoice == '4'):
         print("Chose command 4 - Not Yet Implemented.\nExiting...\n")
         exit(0)
 
-    elif userChoice == '5':
+    elif (userChoice == '5'):
         print("Chose command 5 - Not Yet Implemented.\nExiting...\n")
         exit(0)
 
-    elif userChoice == '6':
+    elif (userChoice == '6'):
         print("Chose command 6 - Not Yet Implemented.\nExiting...\n")
         exit(0)
 
-    elif userChoice == '7':
+    elif (userChoice == '7'):
         print("Chose command 7 - Not Yet Implemented.\nExiting...\n")
         exit(0)
 
-    elif userChoice == '8':
+    elif (userChoice == '8'):
         print("Chose command 8 - Not Yet Implemented.\nExiting...\n")
         exit(0)
 
-    elif userChoice == '9':
+    elif (userChoice == '9'):
         print("Chose command 9 - Not Yet Implemented.\nExiting...\n")
         exit(0)
     
     # If the user wants to exit
-    elif userChoice == 'x':
+    elif (userChoice == 'x'):
         print("\nExiting...")
         exit(0)
     
     # Enable to accept capital X as an exit command
-    # elif userChoice == 'X':
+    # elif (userChoice == 'X'):
     #     print("\nExiting...")
     #     exit(0)
 
